9c2d8b440fb872b8d820f6c1b13cfb66d490a213
IntestinalFortitude20 <p.ruckman90@gmail.com>
2022-07-26 14:04:29 -0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Metronome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPlayPause = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.lblTempo = new System.Windows.Forms.Label();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPlayPause
+            // 
+            this.btnPlayPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayPause.Location = new System.Drawing.Point(280, 245);
+            this.btnPlayPause.Name = "btnPlayPause";
+            this.btnPlayPause.Size = new System.Drawing.Size(219, 104);
+            this.btnPlayPause.TabIndex = 0;
+            this.btnPlayPause.Text = "Play / Pause";
+            this.btnPlayPause.UseVisualStyleBackColor = true;
+            this.btnPlayPause.Click += new System.EventHandler(this.btnPlayPause_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(100, 131);
+            this.trackBar1.Maximum = 230;
+            this.trackBar1.Minimum = 30;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(590, 69);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 60;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // lblTempo
+            // 
+            this.lblTempo.AutoSize = true;
+            this.lblTempo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lblTempo.Location = new System.Drawing.Point(560, 39);
+            this.lblTempo.Name = "lblTempo";
+            this.lblTempo.Size = new System.Drawing.Size(117, 40);
+            this.lblTempo.TabIndex = 3;
+            this.lblTempo.Text = "tempo";
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.lblInterval.Location = new System.Drawing.Point(336, 39);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(134, 40);
+            this.lblInterval.TabIndex = 4;
+            this.lblInterval.Text = "Interval";
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnMinus.Location = new System.Drawing.Point(21, 131);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(61, 52);
+            this.btnMinus.TabIndex = 6;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnPlus.Location = new System.Drawing.Point(727, 131);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(61, 52);
+            this.btnPlus.TabIndex = 7;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.lblTempo);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnPlayPause);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlayPause;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label lblTempo;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Metronome
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer metronomeClick = new SoundPlayer(Properties.Resources.MetronomeClick);
+
+        int tempo;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tempo = trackBar1.Value = 120;
+
+            lblTempo.Text = tempo.ToString();
+            
+            timer1.Interval = 60000 / tempo;
+
+            lblInterval.Text = timer1.Interval.ToString();
+        }
+
+        private void btnPlayPause_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled) timer1.Enabled = false;
+            else timer1.Enabled = true; 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            metronomeClick.Play();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            tempo = trackBar1.Value;
+
+            lblTempo.Text = tempo.ToString();
+
+            timer1.Interval = 60000 / tempo;
+
+            lblInterval.Text = timer1.Interval.ToString();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value += 1;
+
+            trackBar1_Scroll(sender, e);
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            trackBar1.Value -= 1;
+
+            trackBar1_Scroll(sender, e);
+        }
+    }
+}
